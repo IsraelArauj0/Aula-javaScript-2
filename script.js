e1c8a0bd62a7d8cd10aa90e4391f1ebbc6c28019
Israel <israel11.araujo@gmail.com>
2024-03-21 09:10:50 -0300

    Conversão de variaveis

--- script.js
+++ script.js
@@ -7,4 +7,24 @@ console.log(nome)
 let disciplina = null;
 console.log(disciplina)
 //com valor
-let aluno="Fiapinho"
+let aluno = "Fiapinho";
+console.log(aluno)
+
+//Conversão
+//float to int
+let numero = 123.456;
+console.log(parseInt(numero))
+
+//string = float
+let numero1 = "123.456";
+console.log(parseFloat(numero1))
+
+//float = string
+let numero2 = 123.456;
+console.log(numero2.toString())
+
+//identificar o tipo da variavel
+let numero3 =123.456;
+let num = numero3.toString()
+let verificar = typeof num;
+console.log(verificar)
